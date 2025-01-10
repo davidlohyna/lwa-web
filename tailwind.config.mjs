6322376a63a07df4cf94725353e28b494892de88
David Lohyna <82677464+davidlohyna@users.noreply.github.com>
2025-01-10 15:42:56 +0100

    Minor updates to config and web

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,29 +1,21 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'blue': {
-        light: '#357ABD',
-        DEFAULT: '#2C3E50',
-        dark: '#15243D', // logo blue
-      },
-      'red': {
-        light: '#E3B3AC', // pink
-        DEFAULT: '#96271F', // logo red
-        dark: '#7D1D17', // logo red-dark
-      },
-      'grey': '#C5BDBA', // logo grey
-      'grey-light': '#EDEDED',
-      'beige': '#F5EFEA',
-      'green': '#A8D5BA',
-      'orange': '#C49A6C',
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#f4f4f5",
+      blue: "#15243D", // logo blue
+      red: "#96271F", // logo red
+      zinc: colors.zinc,
+      slate: colors.slate,
+      green: colors.emerald,
+      yellow: colors.amber,
+      sky: colors.sky,
     },
     extend: {},
   },
   plugins: [],
-}
+};
